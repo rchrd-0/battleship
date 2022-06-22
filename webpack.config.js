7bfa8ee9d4a1469e7f1818547ac389fbfd7f61d8
richard <richard.ytli+f@gmail.com>
2022-06-22 15:46:05 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'The Odin Project—Battleship',
+      filename: 'index.html',
+    }),
+  ],
+};
