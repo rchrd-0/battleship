d2cfed1a8cbdd84ad570a4e08c74c30343af88d4
richard <richard.ytli+f@gmail.com>
2022-07-07 11:41:33 +0800

    Decouple bot turn taking logic into separate module

---
+++ src/modules/botLogic.js
@@ -0,0 +1,20 @@
+import * as helpers from './helpers';
+
+const generateRandom = () => {
+  const random = [
+    Math.floor(Math.random() * 10),
+    Math.floor(Math.random() * 10),
+  ];
+  return random;
+};
+
+const getRandomMove = async (player) => {
+  let randomMove = generateRandom();
+  while (helpers.alreadyPlayed(player, randomMove)) {
+    randomMove = generateRandom();
+  }
+  await helpers.timeout(400);
+  return randomMove;
+};
+
+export { getRandomMove };
